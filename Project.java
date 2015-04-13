a1b64b7ced7aed2c2153e353a08ef7a2c70c627a
nikkipruitt <nikkipruitt182@gmail.com>
2015-04-12 23:32:28 -0500

    removed some test lines from main

--- Project.java
+++ Project.java
@@ -11,32 +11,17 @@ public class Project {
 		//case One - First Come, First Served order, First Fit allocation policy
 		System.out.println("~~~~~~CASE ONE~~~~~~");
 		Generator caseOne = new Generator(memory, Generator.Order.FCFS, Generator.Allocation.FIRST_FIT);
-	/*	memory.listJobs();
-		memory.listSegments();
-		System.out.println("Finished Jobs: " + caseOne.finishedJobs());
-		System.out.println("Waiting Jobs: " + caseOne.waitingJobs());
-		System.out.println("Wasted Space: " + caseOne.totalWaste());*/
 		caseOne.writeOutput();
 		memory.reset();
 		
 		//case Two - First Come, First Serve order, Best Fit allocation policy
 		System.out.println("~~~~~~CASE TWO~~~~~~");
 		Generator caseTwo = new Generator(memory, Generator.Order.FCFS, Generator.Allocation.BEST_FIT);
-	/*	memory.listJobs();
-		memory.listSegments();
-		System.out.println("Finished Jobs: " + caseTwo.finishedJobs());
-		System.out.println("Waiting Jobs: " + caseTwo.waitingJobs());
-		System.out.println("Wasted Space: " + caseTwo.totalWaste());*/
 		caseTwo.writeOutput();
 		memory.reset();
 		
 		//case Three - Shortest Job First order, Best Fit allocation policy
 		System.out.println("~~~~~~CASE THREE~~~~~~");		Generator caseThree = new Generator(memory, Generator.Order.SJF, Generator.Allocation.BEST_FIT);
-	/*	memory.listJobs();
-		memory.listSegments();
-		System.out.println("Finished Jobs: " + caseThree.finishedJobs());
-		System.out.println("Waiting Jobs: " + caseThree.waitingJobs());
-		System.out.println("Wasted Space: " + caseThree.totalWaste());*/
 		caseThree.writeOutput();
 	}
 
